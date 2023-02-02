cf6d5929b89cd0f51094e9f5fb066c5d660527b0
Anna Zakharova <palmiraptz89@yandex.ru>
2023-02-02 23:55:48 +0300

    fix: fix notes after code review

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
 let editButton = document.querySelector('.profile__edit-button');
-let editPopup = document.querySelector('.profile-edit-popup');
-let closePopupButton = editPopup.querySelector('.profile-edit-popup__close');
+let editPopup = document.querySelector('.popup');
+let closePopupButton = editPopup.querySelector('.popup__close');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-let popupForm = editPopup.querySelector('.profile-edit-popup__form');
+let popupForm = editPopup.querySelector('.profile-edit-form');
 let profileNameInput = popupForm.querySelector('#profile-name');
 let profileJobInput = popupForm.querySelector('#profile-job');
 
 
 function handleEditButtonClick() {
-  editPopup.classList.add('profile-edit-popup_opened');
+  editPopup.classList.add('popup_opened');
 
   profileNameInput.value = profileName.textContent;
   profileJobInput.value = profileJob.textContent;
 }
 
 function handleClosePopupButtonClick() {
-  editPopup.classList.remove('profile-edit-popup_opened');
+  editPopup.classList.remove('popup_opened');
 }
 
 function handlePopupFormSubmit(evt) {
