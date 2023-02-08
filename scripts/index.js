6efa8024ba03b7cd662d1a9f1ef2d7b7ba1b13b6
Anna Zakharova <palmiraptz89@yandex.ru>
2023-02-08 15:05:20 +0300

    feat: add cards from js code, add popup for new card, add icon for card deleting

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,58 @@
-let editButton = document.querySelector('.profile__edit-button');
-let editPopup = document.querySelector('.popup');
-let closePopupButton = editPopup.querySelector('.popup__close');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
+function loadCards() {
+  const cardsContainer = document.querySelector('.cards');
+  const cardTemplate = document.querySelector('#card').content;
 
-let popupForm = editPopup.querySelector('.profile-edit-form');
-let profileNameInput = popupForm.querySelector('#profile-name');
-let profileJobInput = popupForm.querySelector('#profile-job');
-
-
-function handleEditButtonClick() {
-  editPopup.classList.add('popup_opened');
-
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
+  initialCards.forEach(function (item) {
+    const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
+    cardElement.querySelector('.cards__image').src = item.link;
+    cardElement.querySelector('.cards__name').textContent = item.name;
+    cardsContainer.append(cardElement);
+  });
 }
 
-function handleClosePopupButtonClick() {
-  editPopup.classList.remove('popup_opened');
+
+const editButton = document.querySelector('.profile__edit-button');
+const profilePopup = document.querySelector('#profile-popup');
+const closeProfilePopupButton = profilePopup.querySelector('.popup__close');
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
+const profilePopupForm = profilePopup.querySelector('.form');
+const profileNameInput = profilePopupForm.querySelector('#profile-name');
+const profileJobInput = profilePopupForm.querySelector('#profile-job');
+
+function handleEditButtonClick() {
+  profilePopup.classList.add('popup_opened');
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
 }
 
 function handlePopupFormSubmit(evt) {
@@ -26,9 +60,29 @@ function handlePopupFormSubmit(evt) {
 
   profileName.textContent = profileNameInput.value;
   profileJob.textContent = profileJobInput.value;
-  handleClosePopupButtonClick();
+  handleCloseProfilePopupButtonClick();
 }
 
+function handleCloseProfilePopupButtonClick() {
+  profilePopup.classList.remove('popup_opened');
+}
+
+
+const addCardButton = document.querySelector('.profile__add-button');
+const newCardPopup = document.querySelector('#new-card-popup');
+const closeNewCardPopupButton = newCardPopup.querySelector('.popup__close');
+
+function handleAddCardButtonClick() {
+  newCardPopup.classList.add('popup_opened');
+}
+
+function handleCloseNewCardPopupButtonClick() {
+  newCardPopup.classList.remove('popup_opened');
+}
+
+loadCards();
 editButton.addEventListener('click', handleEditButtonClick);
-closePopupButton.addEventListener('click', handleClosePopupButtonClick);
-popupForm.addEventListener('submit', handlePopupFormSubmit);
+profilePopupForm.addEventListener('submit', handlePopupFormSubmit);
+closeProfilePopupButton.addEventListener('click', handleCloseProfilePopupButtonClick);
+addCardButton.addEventListener('click', handleAddCardButtonClick);
+closeNewCardPopupButton.addEventListener('click', handleCloseNewCardPopupButtonClick);
